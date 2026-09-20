5d53e4a7deadd09afde0a65633b3075e533a17c6
TheTarius26 <rhakadewanto26@gmail.com>
2026-09-21 02:03:22 +0700

    feat: release NextLaunch Community Starter (Free / MIT Edition)

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,15 +1,3 @@
-export * from './billing.service';
-export * from './webhook-event.service';
 export * from './workspace.service';
-export * from './impersonation.service';
-export * from './ai.service';
 export * from './auth.service';
-export * from './outgoing-webhook.service';
-export * from './api-key.service';
-export * from './audit-log.service';
-export * from './two-factor.service';
 export * from './session.service';
-
-
-
-
